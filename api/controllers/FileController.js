be1c88a1d560282e7878655746694e329a217612
srinath <vjsrinath@live.com>
2016-02-14 15:24:38 +0530

    initial commit

---
+++ api/controllers/FileController.js
@@ -0,0 +1,156 @@
+/* global GFS */
+/* global Files */
+/**
+ * FileTestApiController
+ *
+ * @description :: Server-side logic for managing Filetestapis
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+import path from 'path';
+
+let streamFile = function (readOptions, req, res) {
+    "use strict";
+
+    //Retrieve file metadata information and then serve the file
+    Files.findOne(readOptions)
+        .then((file, err) = > {
+
+        if (
+    !file || err
+    )
+    {
+        res.notFound("The file doesn't exist!!!");
+        return;
+    }
+
+    if (req.headers['range']) {
+
+        // Range request, partialle stream the file
+        //console.log('Range Reuqest');
+        let parts = req.headers['range'].replace(/bytes=/, "").split("-");
+        let partialstart = parts[0];
+        let partialend = parts[1];
+
+        let start = parseInt(partialstart, 10);
+        let end = partialend ? parseInt(partialend, 10) : file.length - 1;
+        let chunksize = (end - start) + 1;
+
+        console.log('Range ', start, '-', end);
+
+        res.writeHead(206, {
+            'Content-Range': 'bytes ' + start + '-' + end + '/' + file.length,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': file.contentType
+        });
+
+        readOptions.range = {
+            startPos: start,
+            endPos: end
+        };
+    }
+
+    GFS.createReadStream(readOptions, stream = > stream.pipe(res), err =
+    >
+    res.notFound(err)
+    )
+    ;
+})
+    ;
+}
+
+module.exports = {
+
+    download: function (req, res) {
+        "use strict";
+
+        var filename = req.param('filename');
+
+        GFS.createReadStream({
+                filename: filename
+            },
+            stream = > stream.pipe(res),
+            err =
+        >
+        res.notFound(err)
+        )
+        ;
+    },
+
+    isThumbnailReady: function (req, res) {
+        "use strict";
+        var filename = req.param('filename');
+        var readOptions = {filename: path.basename(filename, path.extname(filename) + '.png')};
+        Files.findOne(readOptions)
+            .then((file, err) = > {
+            res.ok({ready: file != undefined, thumb: file && file.filename});
+    })
+        ;
+
+    },
+
+    streamMedia: function (req, res) {
+        "use strict";
+
+        var mediaId = req.param('mediaId');
+
+        Media.findOne({mediaId: mediaId})
+            .then((media, err) = > {
+
+            if (err) {
+                return res.notFound("Media not found!");
+            }
+
+            streamFile({filename: media.file
+    },
+        req, res
+        )
+        ;
+
+    })
+        ;
+    },
+
+    stream: function (req, res) {
+        "use strict";
+        let filename = req.param('filename');
+
+        streamFile({filename: filename}, req, res);
+    },
+
+    upload: function (req, res) {
+        "use strict";
+
+        var blobAdapter = GFS.getSkipper();
+
+        req.file('file')
+            .upload(blobAdapter.receive(), (err, uploadedFiles) = > {
+            if (err) {
+                console.log(err);
+                return res.negotiate(err);
+            }
+            else {
+                var uploadedFile = uploadedFiles[0];
+
+        Thumbnailer.queue(uploadedFile.fd);
+        return res.ok({
+            files: uploadedFiles,
+            textParams: req.params.all()
+        });
+    }
+    })
+        ;
+
+        /*req.file('file').upload({
+         adapter: require('sails-skipper-orientdb'),
+         filesCollection: Files,
+         filesChunksCollection: FilesChunks,
+         maxChunkSize: 512000
+         }, function (err) {
+         res.json(err);
+         });*/
+
+    }
+};
+
---
+++ api/services/GFS.js
@@ -0,0 +1,109 @@
+/**
+ * Created by Srinath Janankiraman on 5/31/15.
+ */
+
+var _blobAdapter, _gfs,
+    Grid = require('gridfs-stream'),
+    fs = require('fs'),
+    path = require('path');
+
+var getFS = function (callback) {
+    "use strict";
+    if (!_gfs) {
+        var mongo = require('mongodb'),
+            mongoClient = mongo.MongoClient;
+
+        mongoClient.connect(sails.config.fileRepositories.gridfs.url,
+            {}, function (err, db) {
+                if (err) throw err;
+
+                _gfs = Grid(db, mongo);
+                // all set!
+                callback(err, _gfs);
+            });
+        return;
+    }
+    callback(undefined, _gfs);
+};
+
+exports.getSkipper = function () {
+    "use strict";
+
+    if (!_blobAdapter) {
+        var gridfs = sails.config.fileRepositories.gridfs;
+        _blobAdapter = require('skipper-gridfs')({uri: gridfs.url + '.' + gridfs.root});
+    }
+    return _blobAdapter;
+};
+
+
+exports.getFS = getFS;
+
+exports.saveFile = function (options, filePath, callback, errCallback) {
+
+
+    if (!options) {
+        options = {};
+    }
+    if (!options.root) {
+        options.root = sails.config.fileRepositories.gridfs.root;
+    }
+
+    getFS((err, gfs) = > {
+
+        let readableStream = fs.createReadStream(filePath)
+
+        "use strict";
+    var outs = gfs.createWriteStream({
+        filename: options.fd,
+        root: options.root,
+        metadata: {
+            fd: options.fd,
+            dirname: path.dirname(options.fd)
+        }
+    });
+    readableStream.once('error', errCallback);
+    outs.once('error', errCallback);
+    outs.once('open', function openedWriteStream() {
+        // console.log('opened output stream for',readableStream.fd);
+        readableStream.extra = _.assign({fileId: this.id}, this.options.metadata);
+    });
+    outs.once('close', function doneWritingFile(file) {
+        // console.log('closed output stream for',readableStream.fd);
+        callback(file);
+
+    });
+    readableStream.pipe(outs);
+})
+    ;
+};
+
+exports.createReadStream = function (options, callback, errCallback) {
+    "use strict";
+
+    if (!options) {
+        options = {};
+    }
+    if (!options.root) {
+        options.root = sails.config.fileRepositories.gridfs.root;
+    }
+
+    getFS((err, fs, db) = > {
+
+        if (err) {
+            errCallback(err);
+            return;
+        }
+
+        var readStream = fs.createReadStream(options);
+
+    readStream.on('error', function (err) {
+        errCallback(err);
+    });
+
+    callback(readStream);
+
+})
+    ;
+
+};
